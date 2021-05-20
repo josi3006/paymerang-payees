08b0b0b769007cc5ab6edfd50fdfb500ad43744f
JGSims <josi3006@hotmail.com>
2021-05-20 13:09:21 -0400

    Included conditional render for components in App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
+import React, { useState } from "react";
 import Payeepage from './components/payeepage';
 import Searchbar from "./components/searchbar.js";
 import Hitlist from './components/searchlist';
 
 function App() {
+
+
+  const [setPayee, Payee] = useState("");
+
+
   return (
+
     <div className="App">
      
 
+     {(Payee=== "") ? <Payeepage /> : <h1>try to post searchlist</h1>}
+
      <Searchbar />
 
-     <Payeepage />
 
      <Hitlist />
 
