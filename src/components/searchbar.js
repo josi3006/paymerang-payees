cf350ea5ac917c83ece1823c35fa19f4f6792f2e
JGSims <josi3006@hotmail.com>
2021-05-20 12:51:41 -0400

    Added component files

---
+++ src/components/searchbar.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+
+
+
+const Searchbar = () => {
+
+
+
+    const msg = "Hello Paymerang!";
+
+
+
+    return (
+
+        <h1>{msg}</h1>
+
+    );
+}
+
+export default Searchbar;
