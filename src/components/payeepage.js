5d0226e3efc831b90313821263ddbdb74a8eb9a5
JGSims <josi3006@hotmail.com>
2021-05-23 12:17:24 -0400

    Built template for Payee Page

--- src/components/payeepage.js
+++ src/components/payeepage.js
@@ -9,27 +9,57 @@ const Payeepage = (props) => {
 
     const msg = "I'm the Payee Page!";
 
-    const showme = (e) => {
-        e.preventDefault();
-        console.log('Payee is set as...');
-        console.log(props.Payee);
-    }
-
-    const data = props.Payee;
+    const data = [props.Payee];
 
     return (
 
         <div>
 
-        <h1>{msg}</h1>
+            <h1>{msg}</h1>
 
-        <button onClick={showme}>Show Me</button>
+            <div className="row" id="big-main-row">
+                <div className="col" id="left-col-with-arrow">
+                    {/* PLACE LEFT ARROW HERE */}
+                </div>
 
-    <h1>{data.Name}</h1>
-    <p>{data.Address.Address1}</p>
-    
+                <div className="col" id="main-middle-col">
 
-        </div>
+                    {/* PAGE CONTENT STARTS HERE */}
+
+
+                    <div className="row" id="payee-basic-info">
+                        <div className="col" id="payee-main-info"></div>
+
+                        <div className="col" id="payee-address"></div>
+                    </div>
+
+                    <div className="row" className="remittance-row">
+                        
+                    </div>
+
+                  
+
+
+
+
+
+                    {/* PAGE CONTENT ENDS HERE */}
+
+                </div>
+
+                <div className="col" id="right-col-with-arrow">
+                    {/* PLACE RIGHT ARROW HERE */}
+                </div>
+
+            </div>
+
+
+
+            <h1>{data.Name}</h1>
+            <p>{data.Address.Address1}</p>
+
+
+        </div >
 
 
     );
--- src/components/searchlist.js
+++ src/components/searchlist.js
@@ -33,42 +33,50 @@ const Hitlist = (props) => {
 
                 <div>
 
-                
-
-                    <h4>Payee Payment Submission Date</h4>
-                        {props.Searchhits.map((hititem) => {
-
-                            let PANarray = (hititem.Payment.PAN.toString()).split("");
-                            let lastfour = PANarray.slice(12, 16).join('');
-                            let hiddenPAN = ("************" + lastfour);
-
-                            return (
-
-                                <div className="row" key={toString(Date.now()) + hititem}>
-
-                                    <div className="col-sm-1"></div>
-
-                                    <div className="col-sm-1 text-left linky" onClick={(e) => choosePayee(e, hititem.Payee)}>
-                                        {hititem.Payee.Name}
-                                    </div>
-
-                                    <div className="col-sm-auto text-left">
-                                        {hiddenPAN}
-                                    </div>
+                    <div className="row mt-3 mb-2">
+                        <div className="col-sm-1"></div>
+                        <div className="col-sm-1 text-left header">Payee</div>
+                        <div className="col-sm-2 text-left header">Payment</div>
+                        <div className="col-sm-auto text-left header">Submission Date</div>
+                    </div>
 
 
-                                    <div className="col-sm-auto text-left">
-                                        {hititem.Payee.SubmissionDate}
-                                    </div>
 
 
+
+                    {props.Searchhits.map((hititem) => {
+
+                        let PANarray = (hititem.Payment.PAN.toString()).split("");
+                        let lastfour = PANarray.slice(12, 16).join('');
+                        let hiddenPAN = ("************" + lastfour);
+
+                        return (
+
+                            <div className="row mt-1" key={toString(Date.now()) + hititem}>
+
+                                <div className="col-sm-1"></div>
+
+                                <div className="col-sm-1 text-left linky" onClick={(e) => choosePayee(e, hititem.Payee)}>
+                                    {hititem.Payee.Name}
+                                </div>
+
+                                <div className="col-sm-2 text-left">
+                                    {hiddenPAN}
                                 </div>
 
 
+                                <div className="col-sm-auto text-left">
+                                    {hititem.Payee.SubmissionDate}
+                                </div>
 
-                            )
-                        })
-                        }
+
+                            </div>
+
+
+
+                        )
+                    })
+                    }
 
 
 
